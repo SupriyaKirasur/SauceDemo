f58fc6b4cc4cad20bf9f1ac66e19314c5fd42ad1
HP <HP@DESKTOP-N7FPAHS>
2023-12-26 18:37:20 +0530

    commit8

--- Test Suites/Launch.ts
+++ Test Suites/Launch.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>Login</name>
+   <name>Launch</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
@@ -10,12 +10,12 @@
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>true</rerunImmediately>
-   <testSuiteGuid>ee99715c-9d1b-46c3-9522-e0d8b8f720c7</testSuiteGuid>
+   <testSuiteGuid>78c036f1-81e4-433b-a287-c11e4b36547a</testSuiteGuid>
    <testCaseLink>
-      <guid>b25e291c-09bd-4eb7-87dc-389f3e2a4609</guid>
+      <guid>bbcc6645-a1d6-46a8-a124-017b21ea7dac</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Login</testCaseId>
+      <testCaseId>Test Cases/Lauch</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
